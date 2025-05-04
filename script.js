cb2807b55373651f82f5762f049b4abf0af07c6c
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-04 13:49:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ let verify2=document.getElementById("verify");
 let para3=document.getElementById("para");
 
 let arrimg=["img1","img2","img3","img4","img5"];
+let random=document.createElement("img");
+main.appendChild(random);
+
