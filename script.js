7505437e48eaa8edc66732ea520a6f54723b0b7d
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-04 13:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let main=document.querySelector("main");
+let reset1=document.getElementById("reset");
+let verify2=document.getElementById("verify");
+let para3=document.getElementById("para");
+
+let arrimg=["img1","img2","img3","img4","img5"];
